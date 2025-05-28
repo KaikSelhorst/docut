66e4ddd11149ac9ce9f7d5d84fb43b727d72c842
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-05-28 18:11:30 -0400

    fix: update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,13 +8,12 @@ export async function middleware(request: NextRequest) {
 
   const { pathname } = new URL(request.url)
 
-  // Dashboard is the root
-  if (!session && pathname === '/') {
-    return NextResponse.redirect(new URL('/sign-in', request.url))
-  }
-
   if (session && authPages.some((v) => v === pathname)) {
     return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  if (!session) {
+    return NextResponse.redirect(new URL('/sign-in', request.url))
   }
 
   return NextResponse.next()
