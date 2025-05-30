f64a6383c52def369b7dedfcebdc123a9b9e0ac0
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-05-30 11:33:28 -0400

    feat: update date lib

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,7 +1,9 @@
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
 
 dayjs.extend(relativeTime)
+dayjs.extend(localizedFormat)
 
 export const dateUtils = {
   format: (date: string | Date, format = 'DD/MM/YYYY') => {
