37a139ad3a76e68489778145109c12d418d71319
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-06-05 11:27:33 -0400

    feat: add root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import { Header } from '@/components/header'
+import { listLinks } from '@/actions/private/list-links'
+import { Suspense } from 'react'
+import { ListLinks, ListLinksSkeleton } from './components/link-table'
 
-export default function Home() {
+interface PageProps {
+  searchParams: Promise<Record<string, string>>
+}
+
+export default async function Home(props: PageProps) {
+  const searchParams = await props.searchParams
+  const listLinkPromise = listLinks(searchParams)
+
   return (
     <main>
-      <Header />
+      <Suspense
+        fallback={<ListLinksSkeleton />}
+        key={JSON.stringify(searchParams)}
+      >
+        <ListLinks linksPromise={listLinkPromise} />
+      </Suspense>
     </main>
   )
 }
