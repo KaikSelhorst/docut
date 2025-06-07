5c95a6cb76c02f0d49d9e44fd2b2c261d8f30439
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-06-07 14:55:32 -0400

    feat: add metadata in the auth pages

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,9 @@
 import { SignInForm } from '../auth-form'
 
+export const metadata = {
+  title: 'Sign-in'
+}
+
 export default function Page() {
   return (
     <section className="h-screen flex justify-center items-center">
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,5 +1,9 @@
 import { SignUpForm } from '../auth-form'
 
+export const metadata = {
+  title: 'Sign-up'
+}
+
 export default function Page() {
   return (
     <section className="h-screen flex justify-center items-center">
