5228458ac38ee27c1b82f281052cabc7fdc80b1e
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-06-10 16:36:38 -0400

    feat: update dashboard actions

---
+++ src/actions/dashboard/link/list-links.ts
@@ -0,0 +1,38 @@
+'use server'
+
+import { getSession } from '@/actions/get-session'
+import { makeRequestQuery } from '@/actions/request'
+import { failure, success } from '@/actions/response'
+import { env } from '@/shared/env'
+
+interface Link {
+  id: string
+  url: string
+  clicks: number
+  expiration: string | null
+  createdAt: string
+  updatedAt: string
+}
+
+interface Paginate {
+  per_page: number
+  total: number
+  total_pages: number
+}
+
+interface Success extends Paginate {
+  links: Link[]
+}
+
+export async function listLinks(filters: Record<string, string>) {
+  const { header } = await getSession()
+
+  const query = makeRequestQuery(filters)
+
+  const res = await fetch(`${env.BETTER_AUTH_URL}/api/private/link${query}`, {
+    headers: { Cookie: header }
+  })
+
+  if (res.status !== 200) return failure(res)
+  return success<Success>(res)
+}
---
+++ src/actions/dashboard/link/update-link.ts
@@ -0,0 +1,37 @@
+'use server'
+
+import { env } from '@/shared/env'
+import { getSession } from '../../get-session'
+import { failure, success } from '../../response'
+
+interface Success {
+  id: string
+  url: string
+  clicks: number
+  expiration: string | null
+  seo: { description: string | null; title: string | null }
+  createdAt: string
+  updatedAt: string
+}
+
+interface Link {
+  expiration: string | null
+  url: string
+  seo: {
+    description: string | null
+    title: string | null
+  }
+}
+
+export async function updateLink(id: string, link: Link) {
+  const { header } = await getSession()
+
+  const res = await fetch(`${env.BETTER_AUTH_URL}/api/private/link/${id}`, {
+    method: 'PATCH',
+    headers: { Cookie: header },
+    body: JSON.stringify(link)
+  })
+
+  if (res.status !== 200) return failure(res)
+  return success<Success>(res)
+}
