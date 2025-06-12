5d1e92c06498576424e3f7de3f0be5ba3a4114f0
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-06-12 18:58:42 -0400

    feat: add emailVerification config in the better auth config

--- src/shared/lib/auth/config.ts
+++ src/shared/lib/auth/config.ts
@@ -1,13 +1,16 @@
+import { makeEmail } from 'server/helpers/email/email'
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { nextCookies } from 'better-auth/next-js'
 import { db } from 'server/db'
 import { makePasswordHasher } from 'server/helpers/cryptography/password'
+import { enableEmailVerification } from 'shared/env'
 
 // Better auth use User, account,
 // verification and session database tables.
 
 const passwordHasher = makePasswordHasher()
+const email = await makeEmail()
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -17,7 +20,22 @@ export const auth = betterAuth({
     enabled: true,
     minPasswordLength: 8,
     maxPasswordLength: 128,
+    requireEmailVerification: enableEmailVerification,
     password: { hash: passwordHasher.hash, verify: passwordHasher.compare }
   },
+  emailVerification: {
+    sendVerificationEmail: async ({ user, url }) => {
+      email.send({
+        to: user.email,
+        from: 'noreply@docut.xyz',
+        subject: 'Complete Sign-up',
+        html: `To complete your sign up access <a href="${url}">this link</a>`
+      })
+    },
+    sendOnSignUp: enableEmailVerification,
+    autoSignInAfterVerification: true,
+    expiresIn: 3600
+  },
+
   plugins: [nextCookies()]
 })
