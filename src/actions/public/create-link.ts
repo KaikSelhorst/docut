493799a74049a5ae43a56ecac94fda7e22539b5e
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-06-28 17:11:01 -0400

    feat: add create link public action

---
+++ src/actions/public/create-link.ts
@@ -0,0 +1,36 @@
+'use server'
+
+import { env } from '@/shared/env'
+import { getSession } from '../get-session'
+import { failure, success } from '../response'
+
+interface Success {
+  id: string
+  url: string
+  clicks: number
+  expiration: string | null
+  createdAt: string
+  updatedAt: string
+}
+
+interface Link {
+  expiration: string | null
+  url: string
+  seo: {
+    description: string | null
+    title: string | null
+  }
+}
+
+export async function createLink(link: Link) {
+  const { header } = await getSession()
+
+  const res = await fetch(`${env.BETTER_AUTH_URL}/api/link`, {
+    method: 'POST',
+    headers: { Cookie: header },
+    body: JSON.stringify(link)
+  })
+
+  if (res.status !== 201) return failure(res)
+  return success<Success>(res)
+}
