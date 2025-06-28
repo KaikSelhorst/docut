2e486a76b4051a7861fb129533f88da2bb3eaec2
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-06-28 17:33:45 -0400

    fix: update body classNames

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: Layout) {
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
-          'min-h-screen bg-background font-sans antialiased',
+          'min-h-svh bg-background font-sans antialiased',
           fontSans.variable,
           fontMono.variable
         )}
