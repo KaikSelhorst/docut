54b3ecb64e6cfa4a315490f7351a265fe5fac46d
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-06-28 19:50:32 -0400

    refactor: change clean-trash route to cleanup

--- src/app/api/jobs/cleanup/route.ts
+++ src/app/api/jobs/cleanup/route.ts
@@ -1,7 +1,7 @@
-import { ClearTrash } from 'server/services/private/trash/clear-trash'
-import { routeAdapter } from '../../route-adapter'
 import { db } from 'server/db'
 import { LinkRepository } from 'server/repository/link-repository'
+import { ClearTrash } from 'server/services/private/trash/clear-trash'
+import { routeAdapter } from '../../route-adapter'
 
 const clearTrash = new ClearTrash(db, new LinkRepository())
 
