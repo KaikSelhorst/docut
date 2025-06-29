38bf4c34a6f2bfa5d83bed81ddc37e5912293b90
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-06-29 17:30:28 -0400

    fix: update robots and sitemap to use new pages

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: ['/$'],
+        allow: ['/$', '/dashboard', '/sign-in', '/sign-up'],
         disallow: ['/']
       }
     ],
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,6 +7,24 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1.0
+    },
+    {
+      url: 'https://www.docut.xyz/dashboard',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1.1
+    },
+    {
+      url: 'https://www.docut.xyz/sign-in',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1.5
+    },
+    {
+      url: 'https://www.docut.xyz/sign-up',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1.6
     }
   ]
 }
