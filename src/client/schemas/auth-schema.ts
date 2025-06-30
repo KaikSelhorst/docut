1abaf4bb09d652e1ce8fa405afef45af6de915b9
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-06-29 20:51:12 -0400

    feat: add forgot and reset password schema

--- src/client/schemas/auth-schema.ts
+++ src/client/schemas/auth-schema.ts
@@ -14,3 +14,17 @@ export const signUpForm = signInSchema.extend({
   name: nameValidator(),
   password: passwordValidator().strong
 })
+
+export const forgotPasswordSchema = z.object({
+  email: emailValidator()
+})
+
+export const resetPasswordSchema = z
+  .object({
+    password: passwordValidator().strong,
+    confirm: z.string()
+  })
+  .refine((data) => data.password === data.confirm, {
+    message: "Passwords don't match",
+    path: ['confirm']
+  })
