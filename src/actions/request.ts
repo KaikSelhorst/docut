3c6c68457259ab681e5f0dfc953e6a35fd94907d
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-07-05 21:27:40 -0400

    fix: update makeRequestQuery

--- src/actions/request.ts
+++ src/actions/request.ts
@@ -1,10 +1,17 @@
-export function makeRequestQuery(value: Record<string, string> | string) {
+export function makeRequestQuery(
+  value: Record<string, string | undefined> | string
+) {
   if (typeof value === 'string') {
     const searchParams = new URLSearchParams(value)
     return `?${searchParams.toString()}`
   }
 
-  const searchParams = new URLSearchParams(value)
+  const searchParams = new URLSearchParams()
+
+  for (const [k, v] of Object.entries(value)) {
+    if (!v) return
+    searchParams.set(k, v)
+  }
 
   return `?${searchParams.toString()}`
 }
