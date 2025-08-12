8b3f56a2235867e5efdbacc775a2ce1b8a4fdbe3
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-08-12 13:01:08 -0400

    feat: enhance middleware routing logic and expand auth route coverage

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,34 @@
 import { getSessionCookie } from 'better-auth/cookies'
 import { type NextRequest, NextResponse } from 'next/server'
 
-const authPages = ['/sign-in', '/sign-up']
-const authenticatedPages = ['/dashboard', '/account']
+const ROUTES = {
+  auth: ['/sign-in', '/sign-up', '/reset-password', '/forgot-password'],
+  protected: ['/dashboard', '/account']
+} as const
 
 export async function middleware(request: NextRequest) {
   const session = getSessionCookie(request)
+  const { pathname } = request.nextUrl
 
-  const { pathname } = new URL(request.url)
-
-  if (session && authPages.some((v) => v === pathname)) {
+  // @ts-expect-error typescript LOL
+  if (session && ROUTES.auth.includes(pathname)) {
     return NextResponse.redirect(new URL('/dashboard', request.url))
   }
 
-  if (
-    !session &&
-    authenticatedPages.some((page) => pathname.startsWith(page))
-  ) {
+  if (!session && ROUTES.protected.some((page) => pathname.startsWith(page))) {
     return NextResponse.redirect(new URL('/sign-in', request.url))
   }
 
   return NextResponse.next()
 }
 
-export const config = { matcher: ['/dashboard/:path*', '/account/:path*'] }
+export const config = {
+  matcher: [
+    '/sign-in/:path*',
+    '/sign-up/:path*',
+    '/reset-password/:path*',
+    '/forgot-password/:path*',
+    '/dashboard/:path*',
+    '/account/:path*'
+  ]
+}
