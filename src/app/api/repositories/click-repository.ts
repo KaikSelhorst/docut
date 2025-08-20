2b197f19fe924918d520c1c075f3db28eaf73e92
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-08-20 17:41:52 -0400

    feat: implement ClickRepository with click registration functionality

---
+++ src/app/api/repositories/click-repository.ts
@@ -0,0 +1,27 @@
+import type { DBInstance } from '@api/db'
+import { type Click, click } from '@api/db/schemas'
+import type { Logger } from '@api/helpers'
+import { and, eq, sql } from 'drizzle-orm'
+
+export class ClickRepository {
+  constructor(private readonly logger: Logger) {}
+
+  async registerClick(tx: DBInstance, clickValue: Omit<Click, 'isUnique'>) {
+    try {
+      const recentClick = await tx.$count(
+        click,
+        and(
+          eq(click.linkId, clickValue.linkId),
+          eq(click.ipAddress, clickValue.ipAddress),
+          sql`${click.createdAt} >= now() - interval '24 hours'`
+        )
+      )
+
+      await tx.insert(click).values({ ...clickValue, isUnique: !recentClick })
+      return true
+    } catch (e) {
+      this.logger.error(`Failed to registry for link with ID: ${clickValue.id}`)
+      return false
+    }
+  }
+}
