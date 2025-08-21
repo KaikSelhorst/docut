fbf540843e1764653fe32345a05de3bc4e9ed6f5
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-08-21 07:45:19 -0400

    fix: update fake route to use real link data

--- src/app/fake/[hash]/route.ts
+++ src/app/fake/[hash]/route.ts
@@ -1,57 +1,67 @@
+import { success } from '@/actions/response'
 import { createGetPublicLinkService } from '@api/services/factories/link'
 import { isbot } from 'isbot'
+
+interface GetPublicLinkData {
+  url: string
+  seo: {
+    title: string
+    description: string
+  }
+}
 
 export const GET = async (
   req: Request,
   { params }: { params: Promise<{ hash: string }> }
 ) => {
-  console.log(params)
   const is = isbot(req.headers.get('User-Agent'))
 
+  const getPublicLinkService = createGetPublicLinkService()
+
+  const { hash } = await params
+
+  const res = await getPublicLinkService.execute(req, { id: hash })
+
+  if (!res.ok) return new Response(null, { status: res.status })
+
+  const { data: linkData } = await success<GetPublicLinkData>(res)
+
   if (is) {
-    const htmlContent = `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Exemplo</title>
-  <!-- Twitter Card metadata -->
-  <meta name="twitter:card" content="summary_large_image">
-  <meta name="twitter:title" content="Exemplo de Prévia">
-  <meta name="twitter:description" content="Este é um HTML retornado pela API com prévia para o Twitter!">
-  <meta name="twitter:image" content="https://exemplo.com/imagem.jpg">
-  <meta name="twitter:site" content="@SeuTwitter">
-  <!-- Open Graph metadata (fallback para Twitter e outras plataformas) -->
-  <meta property="og:title" content="Exemplo de Prévia">
-  <meta property="og:description" content="Este é um HTML retornado pela API com prévia para o Twitter!">
-  <meta property="og:image" content="https://exemplo.com/imagem.jpg">
-  <meta property="og:url" content="https://exemplo.com/api/link">
-  <meta property="og:type" content="website">
-</head>
-<body>
-  <h1>Olá, mundo!</h1>
-  <p>Este é um HTML retornado pela API.</p>
-</body>
-</html>
-  `
+    const htmlContent = generateHTMLTemplate(
+      linkData.seo.title,
+      linkData.seo.description,
+      linkData.url
+    )
     const headers = new Headers()
     headers.set('Content-Type', 'text/html')
 
     return new Response(htmlContent, { headers })
   }
 
-  const { hash } = await params
+  return Response.redirect(linkData.url, 308)
+}
 
-  const getPublic = createGetPublicLinkService()
-
-  const link = await getPublic.execute(req, { id: hash })
-
-  if (link.status === 200) {
-    const json = await link.json()
-    return Response.redirect(json.url, 301)
-  }
-
-  const url = new URL(req.url)
-
-  return Response.redirect(url.origin)
+function generateHTMLTemplate(title: string, description: string, url: string) {
+  return `
+    <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>${title}</title>
+        <!-- Twitter Card metadata -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="${title}">
+        <meta name="twitter:description" content="${description}">
+        <!-- <meta name="twitter:image" content="https://exemplo.com/imagem.jpg"> -->
+        <!-- <meta name="twitter:site" content="@SeuTwitter"> -->
+        <!-- Open Graph metadata (fallback para Twitter e outras plataformas) -->
+        <meta property="og:title" content="${title}">
+        <meta property="og:description" content="${description}">
+        <!-- <meta property="og:image" content="https://exemplo.com/imagem.jpg"> -->
+        <meta property="og:url" content="${url}">
+        <meta property="og:type" content="website">
+      </head>
+      <body></body>
+      </html>
+  `
 }
