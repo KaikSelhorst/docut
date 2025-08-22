f29735d4d92a50ae62f65387e60d64e93d18d6f0
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-08-22 13:49:50 -0400

    feat: return device type in unparser adapter

--- src/app/api/helpers/user-device/adapters/protocols.ts
+++ src/app/api/helpers/user-device/adapters/protocols.ts
@@ -2,5 +2,6 @@ export interface Detecter {
   detect: (userAgent: string) => Promise<{
     browser: { name: string | null }
     os: { name: string | null }
+    device: { type: string | null }
   }>
 }
