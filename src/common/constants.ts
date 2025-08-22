3557dd44180fb155ce137cbee5e4d6e79c8942a8
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-08-22 13:35:32 -0400

    feat: add DESKTOP_OS and MOBILE_OS constant s

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -11,3 +11,35 @@ export const IP_ADDRESS_HEADERS = [
   'forwarded',
   'x-appengine-user-ip'
 ]
+
+export const DESKTOP_OS = [
+  'BeOS',
+  'Chrome OS',
+  'Linux',
+  'Mac OS',
+  'Open BSD',
+  'OS/2',
+  'QNX',
+  'Sun OS',
+  'Windows',
+  'Windows 10',
+  'Windows 2000',
+  'Windows 3.11',
+  'Windows 7',
+  'Windows 8',
+  'Windows 8.1',
+  'Windows 95',
+  'Windows 98',
+  'Windows ME',
+  'Windows Server 2003',
+  'Windows Vista',
+  'Windows XP'
+] as const
+
+export const MOBILE_OS = [
+  'Amazon OS',
+  'Android OS',
+  'BlackBerry OS',
+  'iOS',
+  'Windows Mobile'
+] as const
