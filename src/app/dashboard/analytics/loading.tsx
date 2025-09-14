1c183080761ef56661d3704f3256069900f33938
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2025-09-14 18:11:25 -0400

    feat: enhance Loading component with skeleton placeholders for improved user experience

--- src/app/dashboard/analytics/loading.tsx
+++ src/app/dashboard/analytics/loading.tsx
@@ -1,7 +1,41 @@
+import { Skeleton } from '@/components/ui/skeleton'
+import { cn } from '@/lib/utils'
+
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center grow">
-      <span className="text-muted-foreground">Loading...</span>
-    </div>
+    <main className="px-4 container mx-auto my-6">
+      <div className="mt-3">
+        <Skeleton className="h-[32px] mb-2 w-60" />
+        <Skeleton className="h-[16px] mb-2 w-32" />
+        <Skeleton className="h-[260px]" />
+      </div>
+      <section
+        className={cn(
+          // to much complex
+          'grid mt-12',
+          '[&>*]:border-t',
+          'md:[&>*]:border-l md:grid-cols-2 md:[&>*]:odd:border-l-0',
+          'lg:grid-cols-3 lg:[&>*]:first:border-l-none lg:[&>*]:odd:border-l lg:[&>*]:nth-[3n+1]:border-l-0'
+        )}
+      >
+        <div className="p-4">
+          <Skeleton className="h-[300px]" />
+        </div>
+        <div className="p-4">
+          <Skeleton className="h-[300px]" />
+        </div>
+        <div className="p-4">
+          <Skeleton className="h-[300px]" />
+        </div>
+      </section>
+      <section className="grid md:grid-cols-2 border-t max-md:divide-y md:divide-x">
+        <div className="p-4">
+          <Skeleton className="h-[300px]" />
+        </div>
+        <div className="p-4">
+          <Skeleton className="h-[300px]" />
+        </div>
+      </section>
+    </main>
   )
 }
